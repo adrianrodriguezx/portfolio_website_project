b05b961d4736248c67782537022df94f60c26961
Adrian Rodriguez <contact@adrianrodriguez.us>
2017-01-17 06:05:01 -0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [
+            {
+              width: 320,
+              suffix: '_small',
+              quality: 25
+            },
+            {
+              width: 640,
+              suffix: '_medium',
+              quality: 50
+            },
+            {
+              width: 960,
+              suffix: '_large_1x',
+              quality: 75
+            },
+            {
+              width: 1600,
+              suffix: '_large_2x',
+              quality: 75
+            }
+          ]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['images_src/fixed/*.{gif,jpg,png}'],
+          dest: 'images/',
+          flatten: true,
+        }]
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
+
+
---
+++ README.md
@@ -0,0 +1,32 @@
+#Udacity Project: Portfolio Website
+
+
+##Description
+
+Full responsive portfolio website that displays images, descriptions and links to each portfolio project completed in the Udacity Full Stack Nanodegree program.
+
+
+##Frameworks
+
+  * Bootstrap
+
+##Tools
+
+  * Grunt
+
+##Installation
+
+  * Open index.html with a web browser
+
+##Add new responsive images
+
+  * Save image file(s) in _images_src_
+  * Execute `grunt default` from within root directory in the command line
+  * Custom size images will be saved in the _images_ directory
+  * Use the `media` and `srcset` attributes to specify image quality based on viewport  
+  * Review _Gruntfile.js_ to learn about installed plugins and/or add extra features 
+
+##Licence
+
+_Open Source Software_
+
